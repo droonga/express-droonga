aad3214e7e22afb740e91c9774e9d98f6ba7d78a
YUKI Hiroshi <shimoda@clear-code.com>
2013-01-16 15:42:05 +0900

    Add tests for validation of parameters

--- test/builders-rest-request.test.js
+++ test/builders-rest-request.test.js
@@ -59,6 +59,46 @@ suite('building message from REST API request', function() {
       var actualBody = builders.search({ params: params });
       assert.equalJSON(actualBody, expectedBody);
     });
+
+    suite('validation', function() {
+      function testSuccessFor(key, value, params) {
+        test(key + '=' + value + '(success)', function() {
+          assert.doesNotThrow(function() {
+            params = params || {};
+            params[key] = value;
+            builders.search({ params: params });
+          });
+        });
+      }
+
+      function testFailFor(key, value, params, errorMessage) {
+        test(key + '=' + value + '(fail)', function() {
+          assert.throws(function() {
+            params = params || {};
+            params[key] = value;
+            builders.search({ params: params });
+          }, errorMessage);
+        });
+      }
+
+      var baseParams = { tableName: 'test' };
+
+      testSuccessFor('limit', '0', baseParams);
+      testSuccessFor('limit', '10', baseParams);
+      testSuccessFor('limit', '-10', baseParams);
+      testFailFor('limit', '', baseParams, 'invalid integer');
+      testFailFor('limit', '0.1', baseParams, 'invalid integer');
+      testFailFor('limit', '-0.1', baseParams, 'invalid integer');
+      testFailFor('limit', 'foobar', baseParams, 'invalid integer');
+
+      testSuccessFor('offset', '0', baseParams);
+      testSuccessFor('offset', '10', baseParams);
+      testSuccessFor('offset', '-10', baseParams);
+      testFailFor('offset', '', baseParams, 'invalid integer');
+      testFailFor('offset', '0.1', baseParams, 'invalid integer');
+      testFailFor('offset', '-0.1', baseParams, 'invalid integer');
+      testFailFor('offset', 'foobar', baseParams, 'invalid integer');
+    });
   });
 });
 
