3e5ad8c9953b5d7c0a0af559724841082390d98b
YUKI Hiroshi <shimoda@clear-code.com>
2013-01-16 17:52:48 +0900

    Test registeration of REST API

--- test/rest-api.test.js
+++ test/rest-api.test.js
@@ -53,26 +53,48 @@ suite('REST API', function() {
     function createHandlerFactory(type) {
       return function() {
         return function(request, response) {
-          response.write(type + 'OK');
+          response.contentType('text/plain');
+          response.send(type + ' OK', 200);
         };
       };
     }
+    var handlersFactory = {
+      search: createHandlerFactory('search')
+    };
 
     test('to the document root', function(done) {
       var application = express();
       application.kotoumi({
         prefix:     '',
-        connection: connection,
-        handlers:   {
-          search: createHandlerFactory('search')
-        }
+        connection: 'fake connection',
+        handlers:   handlersFactory
       });
       server = utils.setupServer(application);
 
       utils
         .get('/tables/foobar')
         .next(function(response) {
-          assert.equal('search OK', response.Body);
+          assert.equal('search OK', response.body);
+          done();
+        })
+        .error(function(error) {
+          done(error);
+        });
+    });
+
+    test('under specified path', function(done) {
+      var application = express();
+      application.kotoumi({
+        prefix:     '/path/to/kotoumi',
+        connection: 'fake connection',
+        handlers:   handlersFactory
+      });
+      server = utils.setupServer(application);
+
+      utils
+        .get('/path/to/kotoumi/tables/foobar')
+        .next(function(response) {
+          assert.equal('search OK', response.body);
           done();
         })
         .error(function(error) {
