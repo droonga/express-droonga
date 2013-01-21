1cf762ba64cf29a0e0e88e8a2c0a4637145bb3d2
YUKI Hiroshi <shimoda@clear-code.com>
2013-01-21 15:21:16 +0900

    Add test for Socket.IO API

---
+++ test/socket-api.test.js
@@ -0,0 +1,40 @@
+var assert = require('chai').assert;
+var nodemock = require('nodemock');
+
+var utils = require('./test-utils');
+
+var express = require('express');
+var socketAdaptor = require('../lib/socket-adaptor');
+var Connection = require('../lib/backend-adaptor').Connection;
+
+var client = require('socket.io-client');
+
+suite('Socket.IO API', function() {
+  var server;
+
+  teardown(function() {
+    if (server) {
+      server.close();
+    }
+    server = undefined;
+  });
+
+  test('front to back', function() {
+    var connection = nodemock
+          .mock('emitMessage')
+            .takes('search', { requestMessage: true }, function() {})
+            .ctrl(2, onReceive);
+
+    var application = express();
+    server = utils.setupServer(application);
+    socketAdaptor.registerHandlers(application, server, {
+      connection: connection
+    });
+
+    var clientSocket = client.connect('http://localhost:' + utils.testServerPort);
+    clientSocket.emit('search', { requestMessage: true });
+
+    connection.assertThrow();
+  });
+});
+
