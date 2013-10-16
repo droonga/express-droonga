6f4426c2150bfc675e2cf4b907a2898924c52c03
YUKI Hiroshi <shimoda@clear-code.com>
2013-10-16 17:24:17 +0900

    Fix syntax error

--- test/adapter/socket.io.test.js
+++ test/adapter/socket.io.test.js
@@ -120,7 +120,7 @@ suite('Socket.IO Adapter', function() {
         assert.deepEqual(registeredCommands,
                          [{ name:       'search',
                             definition: scoketIoAPI.search },
-                         [{ name:       'watch',
+                          { name:       'watch',
                             definition: scoketIoAPI.watch },
                           { name:       'getCommand',
                             definition: basePlugin.getCommand },
