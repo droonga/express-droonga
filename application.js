a002886c04b9a312781dc0eea67ce33493c83e19
YUKI Hiroshi <shimoda@clear-code.com>
2013-10-16 18:34:37 +0900

    Don't mount builtin commands by default

--- application.js
+++ application.js
@@ -4,12 +4,23 @@ var express = require('express'),
     droonga = require('./index'),
     http = require('http');
 
+var builtInRestAPI = require('./lib/adapter/api/rest');
+var builtInSocketIoAPI = require('./lib/adapter/api/socket.io');
+var builtInGroongaAPI = require('./lib/adapter/api/groonga');
+var builtInDroongaAPI = require('./lib/adapter/api/droonga');
+
 var application = express();
 var server = http.createServer(application);
 application.droonga({
   prefix: '',
   defaultDataset: 'example',
-  server: server
+  server: server,
+  plugins: [
+    builtInRestAPI,
+    builtInSocketIoAPI,
+    builtInGroongaAPI,
+    builtInDroongaAPI
+  ]
 });
 
 server.listen(13000);
