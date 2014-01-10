ca5b64217532e3d7bd5ebc5de5c013199bd36726
YUKI Hiroshi <shimoda@clear-code.com>
2014-01-10 18:58:49 +0900

    Add mechanism to share session information with socket.io and HTTP connections

--- application.js
+++ application.js
@@ -6,10 +6,22 @@ var express = require('express'),
 
 var application = express();
 var server = http.createServer(application);
+
+var MemoryStore = express.session.MemoryStore;
+var sessionStore = new MemoryStore();
+application.configure(function() {
+  application.use(express.cookieParser('secret key'));
+  application.use(express.session({
+    secret: 'secret key',
+    store:  sessionStore
+  }));
+});
+
 application.droonga({
   prefix: '',
   defaultDataset: 'example',
   server: server,
+  sessionStore: sessionStore, // this is required to share session information by socket.io and HTTP APIs
   plugins: [
     droonga.API_REST,
     droonga.API_SOCKET_IO,
