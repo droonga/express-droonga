4bda50f698bd38c5460bdcb7c1d16d359d254304
YUKI Hiroshi <shimoda@clear-code.com>
2014-03-20 16:06:35 +0900

    Re-introduce "store" as "tryStore",  for testing

--- test/response-cache/entry.test.js
+++ test/response-cache/entry.test.js
@@ -22,38 +22,25 @@ suite('Response Cache Entry', function() {
     });
   });
 
-  function createStubStorage() {
-    return {
-      'set': function() {
-        this.args.push(Array.prototype.slice.call(arguments, 0));
-      },
-      args: []
-    };
-  };
-
-  test('store', function() {
-    var storage = createStubStorage();
-    var entry = new Entry('key', 10, storage);
-    entry.data.status = 200;
-    entry.store();
-    assert.deepEqual(storage.args, [['key', entry.data, 10]]);
-  });
-
-  suite('end', function() {
+  suite('tryStore', function() {
     test('success', function() {
-      var storage = createStubStorage();
-      var entry = new Entry('key', 10, storage);
+      var entry = new Entry('key', 10);
       entry.data.status = 200;
-      entry.store();
-      assert.deepEqual(storage.args, [['key', entry.data, 10]]);
+      var args = [];
+      entry.tryStore(function() {
+        args.push(Array.prototype.slice.call(arguments, 0));
+      });
+      assert.deepEqual(args, [[entry.data, 10]]);
     });
 
     test('error', function() {
-      var storage = createStubStorage();
-      var entry = new Entry('key', 10, storage);
+      var entry = new Entry('key', 10);
       entry.data.status = 400;
-      entry.store();
-      assert.deepEqual(storage.args, []);
+      var args = [];
+      entry.tryStore(function() {
+        args.push(Array.prototype.slice.call(arguments, 0));
+      });
+      assert.deepEqual(args, []);
     });
   });
 });
