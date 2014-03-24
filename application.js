80d11867c908e3bde0a4011e09fb1b0259a67537
Kouhei Sutou <kou@clear-code.com>
2014-03-24 22:22:56 +0900

    Add --droonga-engine-host-name option

--- application.js
+++ application.js
@@ -11,6 +11,8 @@ var version = require('./package.json').version;
 options
   .version(version)
   .option('--port <port>', 'Port number', parseInt, 13000)
+  .option('--droonga-engine-host-name <name>', 'Host name of Droonga engine',
+          '127.0.0.1')
   .option('--droonga-engine-port <port>', 'Port number of Droonga engine',
           parseInt, 24224)
   .option('--enable-logging', 'Enable logging to the standard output')
@@ -42,6 +44,7 @@ application.droonga({
   defaultDataset: 'Droonga',
   server: server,
   sessionStore: sessionStore, // this is required to share session information by socket.io and HTTP APIs
+  hostName: options.droongaEngineHostName,
   port: options.droongaEnginePort,
   plugins: [
     droonga.API_REST,
